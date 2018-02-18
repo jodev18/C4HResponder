44378b34c60e3cbf97ba5c2caa45112c2acbf155
Jojo <jojo@pc.com>
2018-02-19 07:01:20 +0800

    added icons

--- app/src/main/java/dev/jojo/c4hresponder/C4HSplash.java
+++ app/src/main/java/dev/jojo/c4hresponder/C4HSplash.java
@@ -1,18 +1,28 @@
 package dev.jojo.c4hresponder;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class C4HSplash extends AppCompatActivity {
+
+    private Handler h;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_c4_hsplash);
 
-        startActivity(new Intent(getApplicationContext(),RespondLocation.class));
+        h = new Handler(this.getMainLooper());
 
-
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),RespondLocation.class));
+            }
+        },2800);
     }
 }
